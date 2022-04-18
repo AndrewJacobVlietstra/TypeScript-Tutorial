37ff38039040157231494bcb4b70ac492d547c6c
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-04-18 13:40:09 -0400

    Added interface for todos array of todo objects, displaying todo state using json stringify inside a pre element

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import { ChangeEvent, useState } from 'react';
+import ITodo from './Interfaces/Todo.interface';
+import { ArrowFunction } from 'typescript';
 
 
 const App: React.FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
 
     if (name === 'task') {
@@ -18,19 +20,31 @@ const App: React.FC = () => {
     }
   };
 
+  const addTask = (): void => {
+    const newTask = { task, deadline };
+    if (task === '' || deadline <= 0) { return; }
+
+    setTodos([...todos, newTask]);
+
+    setTask("");
+    setDeadline(0);
+  };
+
   return(
     <div className='app-container'>
       <h2 className='app-title'>TypeScript Todo App</h2>
 
       <div className='header'>
         <div className='input-container'>
-          <input type="text" name='task' placeholder='Task...' onChange={handleChange} />
-          <input type="number" name='deadline' placeholder='Deadline (Days)' onChange={handleChange} />
-          <button className='submit-button'>Add Task</button>
+          <input type="text" name='task' placeholder='Task...' onChange={handleChange} value={task} />
+          <input type="number" name='deadline' placeholder='Deadline (Days)' onChange={handleChange} value={deadline} />
+          <button className='submit-button' onClick={addTask}>Add Task</button>
         </div>
       </div>
 
-      <div className='todoList'></div>
+      <div className='todoList'>
+        <pre>{JSON.stringify(todos, undefined, 2)}</pre>
+      </div>
     </div>
   )
 }
