af7512e67f92e314d6113c70f69d242504c1d3f9
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-04-17 22:22:46 -0400

    Made a todo app with typescript, still in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([{}]);
+
+  const [formData, setFormData] = useState({
+    title: "",
+    note: "",
+    time: 0,
+    completed: false,
+  });
+
+  const handleFormDataChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { id, value } = e.currentTarget;
+
+    return setFormData({ ...formData, [id] : value });
+  }
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // return setTodos(...todos, {...formData, id: Math.random()});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TypeScript React Todo List</h1>
+      <pre>{JSON.stringify(todos)}</pre>
+      
+      <form className='app-form' onSubmit={handleFormSubmit}>
+        <label>Todo Title:</label>
+        <input name='title' id='title' type='text' placeholder='Title' value={formData.title} onChange={handleFormDataChange} />
+        <label>Todo Note:</label>
+        <input name='note' id='note' type='text' placeholder='Note' value={formData.note} onChange={handleFormDataChange} />
+        <label>Todo Time (Days):</label>
+        <input name='time' id='time' type='number' value={formData.time} onChange={handleFormDataChange} />
+        <button className='form-button' formAction='submit'>Submit Todo</button>
+      </form>
     </div>
   );
 }
